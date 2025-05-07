db46f55659c2c6696d9b56dffc7b187a23f08062
kibuyagit <159699883+kibuyagit@users.noreply.github.com>
2025-05-07 12:14:25 +0300

    Add files via upload

---
+++ personal_portfolio/function.js
@@ -0,0 +1,11 @@
+// let chat=document.getElementById("submission");
+// let inputs=document.getElementById("inputs");
+// chat.addEventListener("click", function()
+// {
+//     console.log(inputs.value);
+// })
+let hire=document.getElementById("hire");
+hire.addEventListener("click", function()
+{
+    echo="thank you";
+})
